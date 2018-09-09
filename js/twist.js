af947512c2793595cb0d702c5aa8d4b839a1e96a
Syncla <vvazir@udel.edu>
2018-09-09 21:59:39 +0000

    Added functionality to show words left and made words completed nicer Added button to get new word and handled score updates
    Moved all html edits to update function
    Made init consist of function call instead of code

--- js/twist.js
+++ js/twist.js
@@ -3,54 +3,38 @@ var ansrack = [];
 var completedWords = [];
 var colors = [];
 var mainrackString = "";
-
+var scoreAndPairs = "";
 var totalScore = 0;
-var maxScore = 0;
+var maxScore = 100;
+
+/*
+    KEEP THINGS SEPERATE
+    
+    ALL Graphical updates should go in the update section
+    
+    init should only contain function calls, nothing else
+    
+    php scripts should not interact with any html, only the update function should affect html
+    
+    update function should not change any data, it is read only
+    
 
 
-
+*/
 // Needs to be dynamic?
 var unfinishedWords = [];
 
 function init() {
-
-    document.querySelector("#scoreValue").innerText = 0;
-    document.getElementById("ans").addEventListener("keyup", function(evt) {
-
-        var keyID = event.keyCode;
-        var char = String.fromCharCode(keyID);
-        char = char.toUpperCase();
-
-        // Backspace
-        if (keyID == 8) {
-            if (ansrack.length > 0) {
-                mainrack.push(ansrack.pop());
-            }
-        }
-        else if (mainrack.includes(char)) {
-            ansrack.push(char);
-            var idx = mainrack.indexOf(char);
-            mainrack.splice(idx, 1);
-        }
-        else if (keyID == 13) {
-            console.log("Enter");
-            if (ansrack.length > 0) {
-                checkAns();
-            }
-        }
-        update();
-    });
-    // TODO: Call PHP script to get random rack of certain size
+    // Attach the input filtering script to the text box
+    capture();
 
     // TODO: Call PHP to get number and size of results from rack, but not words themselves
 
+    // Get a random rack
     getRandomRack();
-    
-    
 
+    // Get colors to correspond to max scors
 
-
-    colors = gradient("#ff0000","#00FF00",100);
 }
 
 function gradient(startColor, endColor, steps) {
@@ -85,8 +69,36 @@ function gradient(startColor, endColor, steps) {
 
 }
 
+function capture() {
+    document.querySelector("#scoreValue").innerText = 0;
+    document.getElementById("ans").addEventListener("keyup", function(evt) {
 
-function getRandomRack(){
+        var keyID = event.keyCode;
+        var char = String.fromCharCode(keyID);
+        char = char.toUpperCase();
+
+        // Backspace
+        if (keyID == 8) {
+            if (ansrack.length > 0) {
+                mainrack.push(ansrack.pop());
+            }
+        }
+        else if (mainrack.includes(char)) {
+            ansrack.push(char);
+            var idx = mainrack.indexOf(char);
+            mainrack.splice(idx, 1);
+        }
+        else if (keyID == 13) {
+            console.log("Enter");
+            if (ansrack.length > 0) {
+                checkAns();
+            }
+        }
+        update();
+    });
+}
+
+function getRandomRack() {
     // Get rack of random size
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
@@ -94,7 +106,7 @@ function getRandomRack(){
             let rack = JSON.parse(this.response);
             //var table = document.getElementById("rack-storage").getElementsByTagName("tbody")[0];
 
-            
+
             // is this acceptable?
             mainrackString = rack;
             console.log(mainrackString);
@@ -110,37 +122,43 @@ function getRandomRack(){
     xhr.send();
 }
 
+function newWord() {
+    // Get current score
+    var score = totalScore;
+    // Clear the racks
+    var mainLen = mainrack.length;
+    var ansLen = ansrack.length;
+    for (var i = 0; i < mainLen; i++) {
+        mainrack.pop()
+    }
+    for (var i = 0; i < ansLen; i++) {
+        ansrack.pop()
+    }
+    getRandomRack();
 
-function getAnswerScope(){
-    
+}
+
+
+function getAnswerScope() {
+
     // Get number and size of results, plus max Score
-    
-    
-    
+
+
+
     var xhr = new XMLHttpRequest();
     xhr.onload = function() {
         if (this.status == 200) {
             console.log(this.response);
-            let scoreAndPairs = JSON.parse(this.response);
-        
-            
+            scoreAndPairs = JSON.parse(this.response);
+
+
             console.log(scoreAndPairs["totalScore"]);
-            maxScore = scoreAndPairs["totalScore"];
-            
+            // New max score is equal to the current score plus the max from the new words, that will account for changing words
+            maxScore = totalScore + scoreAndPairs["totalScore"];
+
+            colors = gradient("#ff0000", "#00FF00", maxScore);
             console.log(mainrackString.length);
-            for(var answerLength = 2; answerLength <= mainrackString.length; answerLength++)
-            {
-                if(scoreAndPairs[answerLength])
-                {
-                    console.log("Answers of length " + answerLength + " exist and there are: " + scoreAndPairs[answerLength]);
-                }
-            }
-            
-            
-            
-            
-            
-            
+
             update();
         }
     };
@@ -161,22 +179,17 @@ function checkAns() {
                 if (completedWords.indexOf(ans) == -1) {
                     // add ans to completed words
                     completedWords.push(ans);
-
+                    completedWords.sort(function(a, b) {
+                        return (a.length - b.length) || a.localeCompare(b); // sort by dictionary order
+                    });
                     console.log("YAYYY");
 
                     //TODO: Handle score
                     totalScore += parseInt(parseInt(check['weight']));
-                    var color = colors[totalScore];
-                    document.getElementById("scoreValue").style.color = color;
-                    var bar = document.getElementById("score-bar");
-                    if (totalScore<=100){
-                        bar.style.width = totalScore+'%';
-                        bar.style.color = color;
-                    }
-                    // Better way to do this?
 
                     // Clear the answer rack for correct answers
                     var len = ansrack.length;
+                    scoreAndPairs[len]--;
                     for (var i = 0; i < len; i++) {
                         mainrack.push(ansrack.pop());
                     }
@@ -186,11 +199,12 @@ function checkAns() {
                     console.log("Already answered that!");
                 }
                 // We need this because the call is asynchronous it seems?
-                update();
+
             }
             else {
                 console.log("NOES");
             }
+            update();
         }
     };
     xhr.open("GET", "./php/check-if-word.php?ans=" + ans);
@@ -198,11 +212,13 @@ function checkAns() {
 }
 
 function update() {
-    var table = document.getElementById("rack-storage").getElementsByTagName("tbody")[0];
-    if (table.rows.length > 0) {
-        table.deleteRow(0);
+
+    // Update the mainrack and ansrack for new keypresses
+    var racktable = document.getElementById("rack-storage").getElementsByTagName("tbody")[0];
+    if (racktable.rows.length > 0) {
+        racktable.deleteRow(0);
     }
-    var row = table.insertRow(0);
+    var row = racktable.insertRow(0);
     for (var i = 0; i < mainrack.length; i++) {
         var char = document.createTextNode("char");
         var letter = row.insertCell(-1);
@@ -218,11 +234,43 @@ function update() {
     }
     document.getElementById("ans").value = msg;
 
+    // Update the score section, setting color and bar percentage
     document.querySelector("#scoreValue").innerText = String(totalScore);
+    var color = colors[totalScore];
+    document.getElementById("scoreValue").style.color = color;
+    var bar = document.getElementById("score-bar");
+    var percentage = (totalScore / maxScore) * 100;
+    bar.style.width = percentage + '%';
+    bar.style.backgroundColor = color;
 
-    document.querySelector("#answers-toString").innerText = completedWords.toString();
+    //Update words left
+    var info = document.getElementById("ans-info");
+    info.innerHTML = "";
+    for (var answerLength = 2; answerLength <= mainrackString.length; answerLength++) {
+        if (scoreAndPairs[answerLength]) {
+            info.innerHTML += `<tr><td class="ans-cell">${answerLength}</td><td class="ans-cell">${scoreAndPairs[answerLength]}</td></tr>`;
+        }
+    }
 
+
+    // Update completed words section
+    // Super inefficient, but it works
+    var maxCols = 10;
+    var completeTable = document.getElementById("completed-words");
+    // Remove all the contents of the completed words table
+    completeTable.innerHTML = "";
+    var newHTML = "<tr>";
+    for (var i = 0; i < completedWords.length; i++) {
+        if (i % maxCols == 0 && i != 0) {
+            newHTML += "</tr>";
+            if (i + 1 < completedWords.length) {
+                newHTML += "<tr>";
+            }
+        }
+        newHTML += `<td class="ans-cell"> ${completedWords[i]}</td>`;
+    }
+    completeTable.innerHTML = newHTML;
+    //for (var i = 0; i <completedWords.length;i++){
+
+    //}
 }
-
-
-
